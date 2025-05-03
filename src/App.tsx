8467476f3bedc624859fa1fbedb2550bdf24808d
Mariachf <me72068@gmail.com>
2025-05-03 13:11:06 -0300

    Migrando para Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss';
+
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+ <p><small>Coded by <a href="https://www.linkedin.com/in/eduarda-fonseca-109862173/" target="_blank" rel="noopener">Eduarda Fonseca</a> / 2025</small></p>            
+
+               
+
+                
+           
+        </footer>
+    )
+}
